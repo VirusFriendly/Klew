490d8a9319132c6c6d5c7545b5e6904163332157
Gomi <virus.friendly@gmail.com>
2018-08-06 09:30:39 -0400

    Added timeouts

--- hardcidr.py
+++ hardcidr.py
@@ -27,10 +27,8 @@ def apple(dst, port, ttl):
         proto=socket.IPPROTO_ICMP
     )
 
-    try:
-        receiver.bind(('', port))
-    except socket.error as e:  # Todo: Raise better errors
-        raise IOError('Unable to bind receiver socket: {}'.format(e))
+    receiver.bind(('', port))
+    receiver.settimeout(3.0)
 
     # Create sender socket
     sender = socket.socket(
@@ -42,14 +40,9 @@ def apple(dst, port, ttl):
     sender.setsockopt(socket.SOL_IP, socket.IP_TTL, ttl)
     sender.sendto(b'', (dst, port))
 
-    try:
-        # Todo: Add Timeout
-        data, addr = receiver.recvfrom(1024)
-    except socket.error as e:  # Todo: Raise better errors
-        raise IOError('Socket error: {}'.format(e))
-    finally:
-        receiver.close()
-        sender.close()
+    data, addr = receiver.recvfrom(1024)
+    receiver.close()
+    sender.close()
 
     if addr:
         return addr
@@ -57,17 +50,48 @@ def apple(dst, port, ttl):
         raise IOError('Socket error')
 
 
+def log(x):
+    if __debug__:
+        print x
+
+
 if __name__ == '__main__':
+    max_ttl = 30
     x = gen_port()
-    print "Launching apple @ %s" % sys.argv[1]
+    log("Throwing apples @ %s" % sys.argv[1])
 
-    for y in xrange(1, int(sys.argv[2]) + 1):
-        print y,
+    if len(sys.argv) > 2:
+        max_ttl = int(sys.argv[2])
+
+    onion = list()
+
+    for y in xrange(1, max_ttl + 1):
+        log(y)
         hits = list()
+        error = 0
+        hit = ''
 
-        for z in xrange(3):
-            hit = apple(sys.argv[1], x.next(), y)[0]
-            if hit not in hits:
-                hits.append(hit)
+        while len(hits) < 3 or (len([ip for ip in hits if ip == hit]) < 3):
+            try:
+                hit = apple(sys.argv[1], x.next(), y)[0]
+            except socket.timeout:
+                log("*")
+                error = error + 1
 
-        print hits
+                if error > 2:
+                    break
+
+                continue
+            else:
+                if error != 0:
+                    error = 0
+
+            hits.append(hit)
+
+        log([hit for hit in set(hits)])
+        onion.append([hit for hit in set(hits)])
+
+        if sys.argv[1] in hits:
+            break
+
+    print onion[-1]
